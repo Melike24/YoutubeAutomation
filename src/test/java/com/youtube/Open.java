71e3a7665b62b0715d8772d06cc78f9d7c556e24
Melike24 <nurullah24yunus@gmail.com>
2021-02-20 13:07:44 +0100

    Initial commit

---
+++ src/test/java/com/youtube/Open.java
@@ -0,0 +1,29 @@
+package com.youtube;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Open {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromiumdriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.youtube.com/");
+
+        Thread.sleep(3000);
+
+         WebElement a =driver.findElement(By.xpath("//*[@id=\"introAgreeButton\"]/span/span"));
+         a.click();
+         //driver.findElement(By.id("//*[@id=\"introAgreeButton\"]/span/span")).click();
+        //*[@id="introAgreeButton"]/span/span
+        //*[@id="text"]
+        //*[@id="introAgreeButton"]/span/span
+        //*[@id="introAgreeButton"]/span/span
+    }
+}
